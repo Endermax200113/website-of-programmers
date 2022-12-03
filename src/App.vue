325c598bbfcdce7be331f857c14f251bca9943ef
Endermax200113 <47977493+Endermax200113@users.noreply.github.com>
2022-12-03 21:31:50 +0300

    Prepared the site for development

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<div class="site">
+		<Sidebar />
+
+		<div class="site__page">
+			<Header />
+		</div>
+	</div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Sidebar from '@/components/Sidebar.vue';
+import Header from '@/components/Header.vue';
 
-nav {
-  padding: 30px;
+export default {
+	components: {
+		Sidebar, Header
+	},
 }
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style lang="sass">
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+.site 
+	display: flex
+	justify-content: space-between
+	align-items: flex-start
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+	transpileDependencies: true,
+	css: {
+		loaderOptions: {
+			sass: {
+				additionalData: `
+					@import '~@/assets/sass/_normalize.sass'
+					@import '~@/assets/sass/_mixins.sass'
+					@import '~@/assets/sass/_vars.sass'
+					@import '~@/assets/sass/_fonts.sass'
+				`
+			}
+		}
+	}
+});
